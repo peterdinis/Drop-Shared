a55a306a917d118050b9bff018542d6b3ea193a9
Peterdinis <peterdinis0@pobox.sk>
2023-11-18 16:19:39 +0100

    feat: Start working on folder lists WIP

--- app/components/dashboard/DashboardContent.tsx
+++ app/components/dashboard/DashboardContent.tsx
@@ -1,13 +1,20 @@
 import { FC } from "react";
 import Header from "../shared/Header";
+import FolderLists from "../folders/FolderLists";
 
-const DashboardContent: FC = () =>{
-    return (
-        <div className="ml-5 py-4 px-8 overflow-x-auto flex-growtext-center">
-           <Header text="Your files and folders" />
-           
-        </div>
-    )
-}
+const DashboardContent: FC = () => {
+  return (
+    <>
+      <div className="ml-5 py-4 px-8 overflow-x-auto flex-growtext-center">
+        <Header text="Your files and folders" />
+        <hr />
+        <input type="search" name="" placeholder="search" className="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent" />
+        <div className="mt-8 pl-4">
+        <FolderLists />
+      </div>
+      </div>
+    </>
+  );
+};
 
 export default DashboardContent;
