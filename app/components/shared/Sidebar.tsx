a81071a1121c893d8ac354e8cad22f6b91a57b48
Peterdinis <peterdinis0@pobox.sk>
2023-11-18 14:53:53 +0100

    refactor: Making sidebar more responsive WIP

--- app/components/shared/Sidebar.tsx
+++ app/components/shared/Sidebar.tsx
@@ -2,13 +2,11 @@
 
 import { FC, useState } from "react";
 import classNames from "classnames";
-import { FileJson, Folder, BadgeX, XCircle, Menu, LogOut } from 'lucide-react';
+import { FileJson, Folder, BadgeX, XCircle, Menu, LogOut } from "lucide-react";
 import { useAuth } from "../../hooks/useAuthContent";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import AppModal from "./AppModal";
-
-/* TODO: Make more reponsive this component later */
 
 const Sidebar: FC = () => {
   const [collapsed, setSidebarCollapsed] = useState(false);
@@ -39,46 +37,75 @@ const Sidebar: FC = () => {
             <XCircle className="w-7 h-7" />
           )}
         </button>
-        <div>
-          <div className="mt-8">
+        {collapsed === false ? (
+          <>
             <div>
-              <AppModal
-                icon={<FileJson />}
-                btnName={"New file"}
-                headerName={"Upload new file"}
-              >
-                ddd
-              </AppModal>
-            </div>{" "}
-            <br />
-            <div>
-              <AppModal
-                icon={<Folder />}
-                btnName={"New folder"}
-                headerName={"Upload new folder"}
-              >
-                ddd
-              </AppModal>
-            </div>{" "}
-            <div className="mt-4">
-              <BadgeX />{" "}
-              <button
-                className="leading-7 [&:not(:first-child)]:mt-6"
-              >
-                Delete file
-              </button>
+              <div className="mt-8">
+                <div>
+                  <AppModal
+                    icon={<FileJson />}
+                    btnName={"New file"}
+                    headerName={"Upload new file"}
+                  >
+                    ddd
+                  </AppModal>
+                </div>{" "}
+                <br />
+                <div>
+                  <AppModal
+                    icon={<Folder />}
+                    btnName={"New folder"}
+                    headerName={"Upload new folder"}
+                  >
+                    ddd
+                  </AppModal>
+                </div>{" "}
+                <div className="mt-4">
+                  <BadgeX />{" "}
+                  <button className="leading-7 [&:not(:first-child)]:mt-6">
+                    Delete file
+                  </button>
+                </div>
+                <div className="mt-4">
+                  <LogOut onClick={logoutUser} />{" "}
+                  <button
+                    className="leading-7 [&:not(:first-child)]:mt-6"
+                    onClick={logoutUser}
+                  >
+                    Logout
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="mt-4">
-              <LogOut onClick={logoutUser} />{" "}
-              <button
-                className="leading-7 [&:not(:first-child)]:mt-6"
-                onClick={logoutUser}
-              >
-                Logout
-              </button>
+          </>
+        ) : (
+          <div>
+            <div className="mt-8">
+              <div>
+                <AppModal icon={<FileJson />}>ddd</AppModal>
+              </div>{" "}
+              <br />
+              <div>
+                <AppModal icon={<Folder />}>ddd</AppModal>
+              </div>{" "}
+              <div className="mt-4">
+                <BadgeX />{" "}
+                <button className="leading-7 [&:not(:first-child)]:mt-6">
+                  Delete file
+                </button>
+              </div>
+              <div className="mt-4">
+                <LogOut onClick={logoutUser} />{" "}
+                <button
+                  className="leading-7 [&:not(:first-child)]:mt-6"
+                  onClick={logoutUser}
+                >
+                  Logout
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
