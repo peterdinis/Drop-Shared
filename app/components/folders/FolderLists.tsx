021fc36124399ebc33be5a88a2df26908a825152
Peterdinis <peterdinis0@pobox.sk>
2023-11-19 09:45:15 +0100

    feat: New pagination component

--- app/components/folders/FolderLists.tsx
+++ app/components/folders/FolderLists.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
 import FolderCard from "./FolderCard";
+import FolderPagination from "./FolderPagination";
 
 const FolderLists: FC = () => {
   return (
-    <div className="m-auto flex flex-wrap flex-col md:flex-row items-center justify-start">
+    <>
+    roror
+      {/* <div className="m-auto flex flex-wrap flex-col md:flex-row items-center justify-start">
         <FolderCard folderName={"abc"} />
         <FolderCard folderName={"abc"} />
         <FolderCard folderName={"abc"} />
@@ -16,7 +19,9 @@ const FolderLists: FC = () => {
         <FolderCard folderName={"abc"} />
         <FolderCard folderName={"abc"} />
         <FolderCard folderName={"abc"} />
-    </div>
+      </div>
+      <FolderPagination /> */}
+    </>
   );
 };
 
