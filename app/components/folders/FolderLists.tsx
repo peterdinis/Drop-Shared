6ed468d537f53d959f4aa613a969214bf0745ed9
Peterdinis <peterdinis0@pobox.sk>
2023-11-19 17:25:43 +0100

    FOO

--- app/components/folders/FolderLists.tsx
+++ app/components/folders/FolderLists.tsx
@@ -1,8 +1,35 @@
 import { FC } from "react";
 import FolderCard from "./FolderCard";
 import FolderPagination from "./FolderPagination";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "@/app/lib/firebaseConfig";
+import { useAuth } from "@/app/hooks/useAuthContent";
+import { useState } from "react";
 
 const FolderLists: FC = () => {
+  const { currentUser } = useAuth();
+  const [folderList, setFolderList] = useState([]);
+
+  const getFolderList = async () => {
+    setFolderList([]);
+    const q = query(
+      collection(db, "Folders"),
+      where("parentFolderId", "==", 0),
+      where("createBy", "==", currentUser?.email)
+    );
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+      setFolderList((folderList) => [...folderList, doc.data()] as any); // TODO: Fix as any
+    });
+  };
+
   return (
     <>
       <div className="container my-12 mx-auto px-4 md:px-12">
@@ -11,46 +38,46 @@ const FolderLists: FC = () => {
             <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <FolderCard folderName={"abc"} linkName={"test"}  />
+            <FolderCard folderName={"abc"} linkName={"test"} />
           </div>
         </div>
       </div>
