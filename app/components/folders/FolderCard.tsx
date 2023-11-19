556772897b5907b433a514feb05271ba2d571b71
Peterdinis <peterdinis0@pobox.sk>
2023-11-19 10:08:35 +0100

    FOO

--- app/components/folders/FolderCard.tsx
+++ app/components/folders/FolderCard.tsx
@@ -8,43 +8,14 @@ interface IFolderCardProps {
 const FolderCard: FC<IFolderCardProps> = ({ folderName }: IFolderCardProps) => {
   return (
     <article className="overflow-hidden rounded-lg shadow-lg">
-      <a href="#">
-        <img
-          alt="Placeholder"
-          className="block h-auto w-full"
-          src="https://picsum.photos/600/400/?random"
-        />
-      </a>
 
       <header className="flex items-center justify-between leading-tight p-2 md:p-4">
         <h1 className="text-lg">
           <a className="no-underline hover:underline text-black" href="#">
-            Article Title
+            <Folder /> {folderName}
           </a>
         </h1>
-        <p className="text-grey-darker text-sm">11/1/19</p>
       </header>
-
-      <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-        <a
-          className="flex items-center no-underline hover:underline text-black"
-          href="#"
-        >
-          <img
-            alt="Placeholder"
-            className="block rounded-full"
-            src="https://picsum.photos/32/32/?random"
-          />
-          <p className="ml-2 text-sm">Author Name</p>
-        </a>
-        <a
-          className="no-underline text-grey-darker hover:text-red-dark"
-          href="#"
-        >
-          <span className="hidden">Like</span>
-          <i className="fa fa-heart"></i>
-        </a>
-      </footer>
     </article>
   );
 };
--- app/components/folders/FolderLists.tsx
+++ app/components/folders/FolderLists.tsx
@@ -26,6 +26,30 @@ const FolderLists: FC = () => {
         <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
           <FolderCard folderName={"abc"} />
         </div>
+        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+          <FolderCard folderName={"abc"} />
+        </div>
+        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+          <FolderCard folderName={"abc"} />
+        </div>
+        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+          <FolderCard folderName={"abc"} />
+        </div>
+        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+          <FolderCard folderName={"abc"} />
+        </div>
+        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+          <FolderCard folderName={"abc"} />
+        </div>
+        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+          <FolderCard folderName={"abc"} />
+        </div>
+        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+          <FolderCard folderName={"abc"} />
+        </div>
+        <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+          <FolderCard folderName={"abc"} />
+        </div>
       </div>
     </div>
   );
