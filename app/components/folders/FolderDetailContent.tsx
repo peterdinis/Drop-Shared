af1bebfd4e26bece752057cecd9ce324fb6ffe6d
Peterdinis <peterdinis0@pobox.sk>
2023-11-22 17:42:21 +0100

    style: Fixing one style bug

--- app/components/folders/FolderDetailContent.tsx
+++ app/components/folders/FolderDetailContent.tsx
@@ -3,9 +3,9 @@ import Header from "../shared/Header";
 
 const FolderDetailContent: FC = () => {
     return (
-        <>
+        <div className="ml-5 py-4 px-8 flex-growtext-center flex-1">
             <Header text="Folder Detail" />
-        </>
+        </div>
     )
 }
 
