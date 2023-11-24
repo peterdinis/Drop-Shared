fdb914a2e38ae033399a33dee416a94858525c32
Peterdinis <peterdinis0@pobox.sk>
2023-11-24 17:25:27 +0100

    feat: Updating folder detail page

--- app/components/folders/FolderDetailContent.tsx
+++ app/components/folders/FolderDetailContent.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
 import Header from "../shared/Header";
 import Link from "next/link";
+import { Folder } from "lucide-react";
 import { IFolderDetailType } from "@/app/types/folderTypes";
 
 interface IFolderProps {
   folderInfo: IFolderDetailType | any; 
 }
+
+/* TODO: Later display here more information */
 
 const FolderDetailContent: FC<IFolderProps> = ({folderInfo}: IFolderProps) => {
   return (
@@ -14,6 +17,11 @@ const FolderDetailContent: FC<IFolderProps> = ({folderInfo}: IFolderProps) => {
       <span className="mt-4 text-center">
         <Link href="/dashboard">Go to dashboard</Link>
       </span>
+
+      <div className="mt-10">
+        <Folder /> <span>{folderInfo.name.stringValue}</span> <br />
+        <span>Created by: {folderInfo.createdBy.stringValue}</span>
+      </div>
     </div>
   );
 };
