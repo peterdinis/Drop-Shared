0c1cd4cfcd74f6cfaf61831762427d9d3415e522
Peterdinis <peterdinis0@pobox.sk>
2023-11-24 16:54:03 +0100

    feat: Update FolderList

--- app/components/folders/FolderLists.tsx
+++ app/components/folders/FolderLists.tsx
@@ -1,6 +1,5 @@
 import { FC, useEffect, useState } from "react";
 import FolderCard from "./FolderCard";
-import FolderPagination from "./FolderPagination";
 import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "@/app/lib/firebaseConfig";
 import { IFolderType } from "@/app/types/folderTypes";
@@ -10,8 +9,7 @@ import { IFolderType } from "@/app/types/folderTypes";
 const FolderLists: FC = () => {
   const [folders, setFolders] = useState([]);
 
-  const docRef = collection(db, 'Folders');
-
+  const docRef = collection(db, "Folders");
 
   useEffect(() => {
     const unsubscribe = onSnapshot(docRef, (querySnapshot) => {
@@ -25,20 +23,19 @@ const FolderLists: FC = () => {
     return () => unsubscribe();
   }, []);
 
-
   return (
-    <>
-      <div className="container my-12 mx-auto px-4 md:px-12">
-        <div className="flex flex-wrap -mx-1 lg:-mx-4">
-          {folders.map((folder: IFolderType) => (
-            <div key={folder.id} className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-              <FolderCard linkName={folder.id} name={folder.name} />
-            </div>
-          ))}
-        </div>
+    <div className="container my-12 mx-auto px-4 md:px-12">
+      <div className="flex flex-wrap -mx-1 lg:-mx-4">
+        {folders.map((folder: IFolderType) => (
+          <div
+            key={folder.id}
+            className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"
+          >
+            <FolderCard linkName={folder.id} name={folder.name} />
+          </div>
+        ))}
       </div>
-      <FolderPagination />
-    </>
+    </div>
   );
 };
 
