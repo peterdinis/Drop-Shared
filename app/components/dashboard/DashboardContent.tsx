b3d8d28dc7fd41ea22624485a1b9741d9a06bcd0
Peterdinis <peterdinis0@pobox.sk>
2023-12-01 21:24:37 +0100

    FOO

--- app/components/dashboard/DashboardContent.tsx
+++ app/components/dashboard/DashboardContent.tsx
@@ -7,23 +7,21 @@ import ScrollToTop from "react-scroll-to-top";
 
 const DashboardContent: FC = () => {
   return (
-    <>
-      <div className="ml-5 py-4 px-8 flex-growtext-center flex-1">
-        <Header text="Your files and folders" />
-        <br />
-        <input
-          type="search"
-          name=""
-          placeholder="search"
-          className=" bg-white rounded-l border-r border-gray-200 hover:bg-gray-50 active:bg-gray-200 disabled:opacity-50 inline-flex items-center bg-transparent py-1 text-gray-600 px-4 focus:outline-none w-full"
-        />
-        <br />
-        <section className="mt-8">
-          <FolderLists />
-        </section>
-        <ScrollToTop smooth={true} />
-      </div>
-    </>
+    <div className="ml-5 py-4 px-8 flex-growtext-center flex-1">
+      <Header text="Your files and folders" />
+      <br />
+      <input
+        type="search"
+        name=""
+        placeholder="search"
+        className=" bg-white rounded-l border-r border-gray-200 hover:bg-gray-50 active:bg-gray-200 disabled:opacity-50 inline-flex items-center bg-transparent py-1 text-gray-600 px-4 focus:outline-none w-full"
+      />
+      <br />
+      <section className="mt-8">
+        <FolderLists />
+      </section>
+      <ScrollToTop smooth={true} />
+    </div>
   );
 };
 
