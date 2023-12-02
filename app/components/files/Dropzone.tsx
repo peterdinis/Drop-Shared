e028fdf2a7d0cbe5801c6f398748d63bc1620d99
Peterdinis <peterdinis0@pobox.sk>
2023-12-02 09:34:26 +0100

    FOO

--- app/components/files/Dropzone.tsx
+++ app/components/files/Dropzone.tsx
@@ -4,17 +4,30 @@ import Dropzone from "react-dropzone";
 const FileDropzone: FC = () => {
   const maxSize = 20971520;
 
+  /* const handleFileRejections = (fileRejections: any) => {
+    if (fileRejections && fileRejections.length > 0) {
+      console.log("Rejected files:", fileRejections);
+      // You can perform further actions with rejected files here
+    }
+  }; */ /* later */
+
   return (
     <>
-      <Dropzone minSize={0} onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
-        {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+      <Dropzone
+        minSize={0}
+        maxSize={maxSize}
+        onDrop={(acceptedFiles, fileRejections) => {
+          console.log("Accepted files:", acceptedFiles);
+        
+        }}
+      >
+        {({ getRootProps, fileRejections, getInputProps, isDragActive, isDragReject }) => (
           <section>
             <div {...getRootProps()}>
               <input {...getInputProps()} />
               {!isDragActive && "Click here and drop file for upload"}
               {isDragActive && !isDragReject && "Drop to upload this file"}
               {isDragReject && "File is not accepted"}
-              {}
             </div>
           </section>
         )}
