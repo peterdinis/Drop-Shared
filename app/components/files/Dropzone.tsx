e080957e12db60dc166094e725eb867d9f3e0482
Peterdinis <peterdinis0@pobox.sk>
2023-12-02 10:34:29 +0100

    fix: Remove avatar from navbar-links

--- app/components/files/Dropzone.tsx
+++ app/components/files/Dropzone.tsx
@@ -1,15 +1,9 @@
+import { cn } from "@/lib/utils";
 import React, { FC } from "react";
 import Dropzone from "react-dropzone";
 
 const FileDropzone: FC = () => {
   const maxSize = 20971520;
-
-  /* const handleFileRejections = (fileRejections: any) => {
-    if (fileRejections && fileRejections.length > 0) {
-      console.log("Rejected files:", fileRejections);
-      // You can perform further actions with rejected files here
-    }
-  }; */ /* later */
 
   return (
     <>
@@ -22,8 +16,8 @@ const FileDropzone: FC = () => {
         }}
       >
         {({ getRootProps, fileRejections, getInputProps, isDragActive, isDragReject }) => (
-          <section>
-            <div {...getRootProps()}>
+          <section className="m-4">
+            <div className={cn("w-full h-52 flex justify-center items-center p-5 border border-dashed rounded-lg textcenter", isDragActive ? "bg-[#035FFE] text-white animate-pulse" : "bg-slate-100/50 dark:bg-slate-8000/80 text-slate-400")} {...getRootProps()}>
               <input {...getInputProps()} />
               {!isDragActive && "Click here and drop file for upload"}
               {isDragActive && !isDragReject && "Drop to upload this file"}
--- app/components/navbar/NavbarLinks.tsx
+++ app/components/navbar/NavbarLinks.tsx
@@ -3,7 +3,6 @@
 import { FC } from "react";
 import Link from "next/link";
 import { useAuth } from "@/app/hooks/useAuthContent";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const NavbarLinks: FC = () => {
   const { currentUser } = useAuth();
@@ -11,14 +10,7 @@ const NavbarLinks: FC = () => {
   return (
     <>
       {currentUser ? (
-        <>
-          <Avatar>
-            <Link href="/dashboard">
-              <AvatarImage src={currentUser?.photoURL as unknown as string} />
-              <AvatarFallback>User</AvatarFallback>
-            </Link>
-          </Avatar>
-        </>
+        <Link href="/dashboard">{currentUser?.email}</Link>
       ) : (
         <>
           <li className="text-black text-xl">
