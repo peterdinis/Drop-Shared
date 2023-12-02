bca0e51ea0e76d0651494b60d9c3980a97f459e2
Peterdinis <peterdinis0@pobox.sk>
2023-12-02 09:39:30 +0100

    refactor: Cleaning sidebar

--- app/components/shared/Sidebar.tsx
+++ app/components/shared/Sidebar.tsx
@@ -9,17 +9,9 @@ import { useRouter } from "next/navigation";
 import AppModal from "./AppModal";
 import { Button } from "@/components/ui/button";
 import { Tooltip } from "@mui/material";
-import { db } from "@/app/lib/firebaseConfig";
-import { doc, setDoc } from "firebase/firestore";
-import SmallHeader from "./SmallHeader";
-import { v4 as uuid } from "uuid";
 import Cookies from "js-cookie";
 
 const Sidebar: FC = () => {
-  const { currentUser } = useAuth();
-  const docId = uuid().toString() as unknown as string;
-
-  const [folderName, setFolderName] = useState("");
   const [collapsed, setSidebarCollapsed] = useState(false);
 
   const { logout } = useAuth();
@@ -30,16 +22,6 @@ const Sidebar: FC = () => {
     toast.success("Logout successfull");
     Cookies.remove("userCredentials");
     router.push("/login");
-  };
-
-  const onCreateNewFolder = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    await setDoc(doc(db, "Folders", docId), {
-      name: folderName,
-      id: docId,
-      createdBy: currentUser?.email,
-    });
-    toast.success("New folder was created");
   };
 
   return (
@@ -82,25 +64,6 @@ const Sidebar: FC = () => {
                     </form>
                   </AppModal>
                 </div>{" "}
-               {/*  <div className="mt-8">
-                  <AppModal
-                    icon={<Folder />}
-                    btnName={"New folder"}
-                    headerName={"Upload new folder"}
-                  >
-                    <form className="mt-5">
-                      <input
-                        className="bg-white rounded-l border-r border-gray-200 hover:bg-gray-50 active:bg-gray-200 disabled:opacity-50 inline-flex items-center bg-transparent py-1 text-gray-600 px-4 focus:outline-none w-full"
-                        placeholder="New file"
-                        value={folderName}
-                        onChange={(e) => setFolderName(e.target.value)}
-                      />
-                      <div className="mt-3">
-                        <Button onClick={onCreateNewFolder}>Upload</Button>
-                      </div>
-                    </form>
-                  </AppModal>
-                </div>{" "} */}
                 <div className="mt-8">
                   <Button variant={"ghost"} value="sm" onClick={logoutUser}>
                     <LogOut onClick={logoutUser} />
@@ -131,25 +94,6 @@ const Sidebar: FC = () => {
                 </Tooltip>
               </div>{" "}
               <br />
-              {/* <div>
-                <Tooltip title="Add new folder">
-                  <AppModal tooltipName="Add new folder" icon={<Folder />}>
-                    <SmallHeader text="Upload new folder" />
-                    <form className="mt-5">
-                      <input
-                        type="text"
-                        className="bg-white rounded-l border-r border-gray-200 hover:bg-gray-50 active:bg-gray-200 disabled:opacity-50 inline-flex items-center bg-transparent py-1 text-gray-600 px-4 focus:outline-none w-full"
-                        placeholder="New folder"
-                        value={folderName}
-                        onChange={(e) => setFolderName(e.target.value)}
-                      />
-                      <div className="mt-3">
-                        <Button onClick={onCreateNewFolder}>Upload</Button>
-                      </div>
-                    </form>
-                  </AppModal>
-                </Tooltip>
-              </div>{" "} */}
               <div className="mt-8">
                 <Tooltip title="Logout">
                   <Button onClick={logoutUser} variant={"ghost"} size={"sm"}>
