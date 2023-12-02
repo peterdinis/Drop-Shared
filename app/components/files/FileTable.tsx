4ac662a9216b4a86ee552ffc5daa540c05d51606
Peterdinis <peterdinis0@pobox.sk>
2023-12-02 21:20:55 +0100

    FOO

--- app/components/files/FileTable.tsx
+++ app/components/files/FileTable.tsx
@@ -3,16 +3,16 @@ import SmallHeader from "../shared/SmallHeader";
 import { Button } from "@/components/ui/button";
 
 const FileTable: FC = () => {
-    return (
-        <div className="border-t">
-         <SmallHeader text="Your uploaded files" />
-         <div className="mt-3">
-            <section className="container space-y-5">
-                <Button>Sorty by...</Button>
-            </section>
-         </div>
-        </div>
-    )
-}
+  return (
+    <div className="border-t">
+      <SmallHeader text="Your uploaded files" />
+      <div className="mt-3">
+        <section className="container space-y-5">
+          <Button>Sort  by...</Button>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default FileTable;
