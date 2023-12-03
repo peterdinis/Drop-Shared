2df428be97c08ee5662b5b8565f05753ab1161a0
Peterdinis <peterdinis0@pobox.sk>
2023-12-03 08:12:48 +0100

    FOO

---
+++ app/lib/constants.ts
@@ -0,0 +1,8 @@
+export const COLOR_EXTENSION_MAP: { [key: string]: string } = {
+    pdf: "#0000FF",
+    docx: "#008000",
+    doc: "#FFA500",
+    jpg: "#FF0000",
+    jpeg: "#800080",
+    zip: "#8B4513"
+};
