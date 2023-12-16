35560178734cde32e0d2af085a1cfabd22563ae6
Peterdinis <peterdinis0@pobox.sk>
2023-12-16 12:37:09 +0100

    refactor: Refactoring application WIP

--- app/components/dashboard/DashboardContent.tsx
+++ app/components/dashboard/DashboardContent.tsx
@@ -2,7 +2,6 @@
 
 import { FC } from "react";
 import Header from "../shared/Header";
-import FileLists from "../files/FileLists";
 import ScrollToTop from "react-scroll-to-top";
 
 const DashboardContent: FC = () => {
@@ -10,9 +9,6 @@ const DashboardContent: FC = () => {
     <div className="ml-5 py-4 px-8 flex-growtext-center flex-1">
       <Header text="Upload file" />
       <br />
-      <section>
-        <FileLists />
-      </section>
       <ScrollToTop smooth={true} />
     </div>
   );
