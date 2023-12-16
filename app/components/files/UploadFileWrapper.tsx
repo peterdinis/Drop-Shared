4bff527e42ec1b696ab14d02c1ef2d2ae66bc34c
Peterdinis <peterdinis0@pobox.sk>
2023-12-16 17:47:16 +0100

    feat: Writing new upload page

--- app/components/files/UploadFileWrapper.tsx
+++ app/components/files/UploadFileWrapper.tsx
@@ -1,11 +1,20 @@
 import { FC } from "react";
+import Sidebar from "../shared/Sidebar";
+import Header from "../shared/Header";
+import UploadForm from "./UploadForm";
 
 const UploadFileWrapper: FC = () => {
-    return (
-        <>
-        
-        </>
-    )
-}
+  return (
+    <div className="flex text-gray-900 bg-gray-100 dark:bg-dark dark:text-light">
+      <Sidebar />
+      <div className="ml-5 py-4 px-8 flex-growtext-center flex-1">
+        <Header text="Upload new file" />
+        <div className="mt-5">
+            <UploadForm />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default UploadFileWrapper
+export default UploadFileWrapper;
