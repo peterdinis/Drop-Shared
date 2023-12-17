5705a1dfd36bd2437318ccab5b1500e0cc479737
Peterdinis <peterdinis0@pobox.sk>
2023-12-17 14:38:07 +0100

    feat: Upload form work

--- app/components/files/UploadForm.tsx
+++ app/components/files/UploadForm.tsx
@@ -13,6 +13,7 @@ import ProgressBar from "./ProgressBar";
 
 const UploadForm: FC = () => {
   const [file, setFile] = useState<File | null>(null);
+  const [progress, setProgress] = useState(0);
 
   const imageRef = ref(storage, `file-upload/${file?.name}`);
 
@@ -32,6 +33,7 @@ const UploadForm: FC = () => {
       // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
       console.log("Upload is " + progress + "% done");
+      setProgress(progress);
       switch (snapshot.state) {
         case "paused":
           console.log("Upload is paused");
@@ -42,25 +44,9 @@ const UploadForm: FC = () => {
       }
     },
     (error) => {
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case "storage/unauthorized":
-          // User doesn't have permission to access the object
-          break;
-        case "storage/canceled":
-          // User canceled the upload
-          break;
-
-        // ...
-
-        case "storage/unknown":
-          // Unknown error occurred, inspect error.serverResponse
-          break;
-      }
+      console.log(error)
     },
     () => {
-      // Upload completed successfully, now we can get the download URL
       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
         console.log("File available at", downloadURL);
       });
@@ -120,7 +106,7 @@ const UploadForm: FC = () => {
         Upload{" "}
       </Button>
       <div className="mt-6">
-        <ProgressBar />
+        <ProgressBar progress={progress} />
       </div>
 
     </>
