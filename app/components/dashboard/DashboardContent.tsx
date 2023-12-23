8dad642454b3d4ab5c2b97735c7a3d334817d76e
Peterdinis <peterdinis0@pobox.sk>
2023-12-23 12:28:21 +0100

    feat: Start to updating dashboard page

--- app/components/dashboard/DashboardContent.tsx
+++ app/components/dashboard/DashboardContent.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react";
 import Header from "../shared/Header";
 import { useAuth } from "@/app/hooks/useAuthContent";
+import DashboardHelpers from "./DashboardHelpers";
 
 const DashboardContent: FC = () => {
-  const {currentUser} = useAuth();
+  const { currentUser } = useAuth();
 
   return (
-    <div className="ml-5 py-4 px-8 flex-growtext-center flex-1">
+    <div className="ml-5 py-4 px-8 flex-grow text-center flex-1">
       <Header text="My Dashboard" />
       <br />
       <div className="mt-5">
-        <h1>Hello: {currentUser?.email}</h1>
+        <h1 className="shadow-sm font-bold">Hello: {currentUser?.email}</h1>
+        <DashboardHelpers />
       </div>
     </div>
   );
