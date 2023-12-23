0a8bf05f652a7f475adc01763a72b72027a5f74a
Peterdinis <peterdinis0@pobox.sk>
2023-12-23 17:50:17 +0100

    style: Design FileDisplayPreview

--- app/components/files/FIleDisplayPreview.tsx
+++ app/components/files/FIleDisplayPreview.tsx
@@ -1,24 +1,29 @@
 import Image from "next/image";
 import { FC } from "react";
 import ExapleFile from "../../../public/img/exampleFile.png";
+import prettyBytes from "pretty-bytes";
 
 interface IFileDisplayPreview {
-    file?: File;
+  file?: File;
 }
 
-const FileDisplayPreview: FC<IFileDisplayPreview> = ({file}: IFileDisplayPreview) => {
+const FileDisplayPreview: FC<IFileDisplayPreview> = ({
+  file,
+}: IFileDisplayPreview) => {
+  const fileSize = prettyBytes(file?.size as unknown as number);
+
   return (
-    <>
-      <Image
-        src={ExapleFile}
-        alt={"Default file image"}
-        width={50}
-        height={50}
-      />
-      <div className="">
-        <h2>File Name: {file?.name || "Test File"}</h2>
+    <div className="flex items-center gap-2 justify-between">
+      <div className="flex items-center p-2">
+        <Image src={ExapleFile} alt={"Default file image"} width={50} />
+        <div className="text-left">
+          <h2 className="p-1 font-bold text-[12px]">{file?.name}</h2>
+          <h2 className="p-1 font-bold text-gray-500">
+            {file?.type} - {fileSize}
+          </h2>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
