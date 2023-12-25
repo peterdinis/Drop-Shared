a4ec0554454ad85bb5cd8fc8bb18a23dab7d7056
Peterdinis <peterdinis0@pobox.sk>
2023-12-25 16:35:02 +0100

    refactor: Updating UploadForm component

--- app/components/files/UploadForm.tsx
+++ app/components/files/UploadForm.tsx
@@ -3,52 +3,26 @@
 import { Button } from "@/components/ui/button";
 import { FC, useState } from "react";
 import { toast } from "react-hot-toast";
-import { storage, db } from "@/app/lib/firebaseConfig";
-import {
-  ref,
-} from "firebase/storage";
+import { storage } from "@/app/lib/firebaseConfig";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { Upload } from "lucide-react";
-import {  doc,serverTimestamp,setDoc } from "firebase/firestore";
-import { useAuth } from "@/app/hooks/useAuthContent";
-import { UploadedFile } from "@/app/types/fileTypes";
-import { generateRandomString } from "@/app/utils/randomString";
 import FileDisplayPreview from "./FIleDisplayPreview";
-
+import { v4 } from "uuid";
 
 const UploadForm: FC = () => {
   const [file, setFile] = useState<File | null>(null);
-  const {currentUser} = useAuth();
+  const [, setImageUrls] = useState([]);
 
-  const docId = serverTimestamp() as unknown as string;
-
-  const imageRef = ref(storage, `file-upload/${file?.name}`);
-
-  const saveFile = async (file: File, fileUrl: string) => {
-
-    const fileDocRef = doc(db, 'uploadedFile', docId);
-    
-    const fileData: UploadedFile = {
-        fileName: file.name,
-        fileSize: file.size,
-        fileType: file.type,
-        fileUrl,
-        userEmail: currentUser?.email!,
-        shortUrl: process.env.NEXT_PUBLIC_BASE_URL as unknown as string + generateRandomString(13)
-    };
-
-    await setDoc(fileDocRef, fileData);
-};
-
-  const handleUploadFile = (e: any) => {
-    if (e.target.files[0]!?.size > 200000) {
-      toast.error("File is too big");
-      return;
-    }
-
-    setFile(e.target.files[0]!);
-    toast.success("File was uploaded");
+  const handleUploadFile = () => {
+    if (file == null) return;
+    const imageRef = ref(storage, `uploaded-images/${file.name + v4()}`);
+    uploadBytes(imageRef, file).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url] as any);
+      });
+      toast.success("File was uploaded");
+    });
   };
-
 
   return (
     <>
@@ -58,9 +32,7 @@ const UploadForm: FC = () => {
           className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
         >
           <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <Upload
-              className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-            />
+            <Upload className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" />
             <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
               <span className="font-semibold">Click to upload</span> or drag and
               drop
@@ -70,7 +42,9 @@ const UploadForm: FC = () => {
             </p>
           </div>
           <input
-            onChange={handleUploadFile}
+            onChange={(event) => {
+              setFile(event.target.files![0]);
+            }}
             id="dropzone-file"
             type="file"
             className="hidden"
@@ -78,10 +52,10 @@ const UploadForm: FC = () => {
         </label>
       </div>
       <div className="mt-5">
-        {file? <FileDisplayPreview file={file as unknown as File} /> : null}
+        {file ? <FileDisplayPreview file={file as unknown as File} /> : null}
       </div>
       <br />
-      <Button disabled={!file} className="mt-5">
+      <Button onClick={handleUploadFile} className="mt-5">
         Upload{" "}
       </Button>
     </>
