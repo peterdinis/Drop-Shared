eee02658e88b8ee64e0997e2205c8608209885c2
Peterdinis <peterdinis0@pobox.sk>
2023-12-26 13:45:03 +0100

    style: Add missing class

--- app/components/files/FileLists.tsx
+++ app/components/files/FileLists.tsx
@@ -23,7 +23,7 @@ const FileLists: FC = () => {
   }, []);
 
   return (
-    <div className='flex text-gray-900 bg-gray-100 dark:bg-dark dark:text-light'>
+    <div className='overflow-x-hidden flex text-gray-900 bg-gray-100 dark:bg-dark dark:text-light'>
       <Sidebar />
       <div className='ml-5 py-4 px-8 flex-growtext-center flex-1'>
         <Header text='My uploaded files' />
