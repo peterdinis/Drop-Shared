7325a9f6cb0ab26626a2a2532d72bf22d1e0b7b5
Peterdinis <peterdinis0@pobox.sk>
2023-12-26 20:15:33 +0100

    feat: Update toast duration in SharedFileForm

--- app/components/files/SharedFileForm.tsx
+++ app/components/files/SharedFileForm.tsx
@@ -43,7 +43,7 @@ const SharedFileForm: FC = () =>{
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </pre>
       ),
-      duration: 2000,
+      duration: 4000,
     })
   }
  
