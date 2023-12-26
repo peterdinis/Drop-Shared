ca022d61ebd6480c7e7fac0f950c0154f30f8916
Peterdinis <peterdinis0@pobox.sk>
2023-12-26 13:25:50 +0100

    Writing new dialog component for sharing files WIP

--- app/components/files/SharedFileDialog.tsx
+++ app/components/files/SharedFileDialog.tsx
@@ -1,11 +1,38 @@
-import { FC } from "react";
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { FC, useState } from 'react';
+import Header from '../shared/Header';
 
 const SharedFileDialog: FC = () => {
-    return (
-        <>
-        
-        </>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(v) => {
+        if (!v) {
+          setIsOpen(v);
+        }
+      }}
+    >
+      <DialogTrigger
+        onClick={() => {
+          setIsOpen(true);
+        }}
+        asChild
+      >
+        <Button variant={'default'} size='lg'>
+          Shared with friend
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <Header text='Share this file with your friend' />
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default SharedFileDialog;
