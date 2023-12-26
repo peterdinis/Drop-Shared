05b4a2714ff7345dfce0b02892788ceaed5ab864
Peterdinis <peterdinis0@pobox.sk>
2023-12-26 20:28:18 +0100

    FOO

--- app/components/navbar/NavbarLinks.tsx
+++ app/components/navbar/NavbarLinks.tsx
@@ -7,9 +7,11 @@ import { useAuth } from '@/app/hooks/useAuthContent';
 const NavbarLinks: FC = () => {
   const { currentUser } = useAuth();
 
+  const actualPath = window.location.pathname;
+
   return (
     <>
-      {currentUser === null ? (
+      {currentUser === null && actualPath === "/login" ? (
         <>
           <li className='text-black text-xl'>
             <Link href='/login'>Login</Link>
