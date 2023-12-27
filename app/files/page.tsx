9af5c48d5f38c42ac0c73c75f637c8decff49696
Peterdinis <peterdinis0@pobox.sk>
2023-12-27 08:55:56 +0100

    infra: Adding private routes to two pages

--- app/files/page.tsx
+++ app/files/page.tsx
@@ -1,8 +1,9 @@
 import { NextPage } from 'next';
 import FileLists from '../components/files/FileLists';
+import PrivateRoute from '../routes/PrivateRoute';
 
 const AllFilesPage: NextPage = () => {
   return <FileLists />;
 };
 
-export default AllFilesPage;
+export default PrivateRoute(AllFilesPage);
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,8 +1,9 @@
 import { NextPage } from 'next';
 import UploadFileWrapper from '../components/files/UploadFileWrapper';
+import PrivateRoute from '../routes/PrivateRoute';
 
 const UploadPage: NextPage = () => {
   return <UploadFileWrapper />;
 };
 
-export default UploadPage;
+export default PrivateRoute(UploadPage);
